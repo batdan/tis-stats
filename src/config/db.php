c3b83731995daf81051c96bafc08c1235844d2b5
Daniel Gomes <daniel.gomes.44@gmail.com>
2021-08-21 19:08:31 +0200

    [UPD] conf

--- src/config/db.php
+++ src/config/db.php
@@ -2,12 +2,6 @@
 /**
  * Connexion aux bases de données
  */
-return [
-    'default' => [
-        'charset' => 'utf8mb4',
-        'host' => 'localhost',
-        'base' => 'tis_stats',
-        'user' => 'tis_stats',
-        'pass' => '37yDUkp8mlIweboAHd7JJuJi',
-    ]
-];
+include ( __DIR__ . '/../env.php' );
+
+return $configDb;
