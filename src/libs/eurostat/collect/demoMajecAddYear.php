9096de40bfb637990b949338a5a9f2e16a996b0a
Daniel Gomes <gomes.daniel.44@gmail.com>
2021-10-19 13:31:45 +0200

    [ADD] magec calc last year

--- src/libs/eurostat/collect/demoMajecAddYear.php
+++ src/libs/eurostat/collect/demoMajecAddYear.php
@@ -11,12 +11,13 @@ class demoMajecAddYear
 {
     private $schema = 'tis_stats';
 
-    private $year = 2020;
+    private $year = 2020;       // Année à rattaper
+    private $nbDaysWeek1;       // Nombre de jours dans la permière semaine de l'année
+    private $nbDaysWeek53;      // Nombre de jours dans la permière semaine de l'année
 
     private $keysAges;
     private $keysCountries;
     private $magecExistYear;
-
 
     public function __construct()
     {
@@ -26,18 +27,8 @@ class demoMajecAddYear
         $this->getKeysAges();
         $this->getKeysCountries();
         $this->checkMagecExistYear();
+        $this->nbDaysFirstLastWeek();
         $this->processData();
-
-        // echo '<pre>';
-        // print_r($this->keysCountries);
-        // echo chr(10);
-        // echo chr(10);
-        // var_dump($this->magecExistYear);
-        // echo chr(10);
-        // echo chr(10);
-        // echo '</pre>';
-
-
     }
 
 
@@ -69,26 +60,30 @@ class demoMajecAddYear
 
     private function processData()
     {
+        if ($this->magecExistYear) {
+            return;
+        }
+
         $geotime = [];
         foreach($this->keysCountries as $country) {
             $geotime[] = "'" . $country . "'";
         }
         $geotime = implode(',', $geotime);
 
-        echo chr(10);
-        echo chr(10);
-        echo 'Test : ' . $geotime;
-        echo chr(10);
-        echo chr(10);
-
-        return;
-
-        $req = "SELECT  *
+        // Récupération des données de la dernière années dans la table "eurostat_demo_r_mwk_05"
+        $req = "SELECT  sex, age, geotime, year_week, value
                 FROM    eurostat_demo_r_mwk_05
                 WHERE   geotime IN ($geotime)
-                AND     year    = ";
+                AND     age != :age
+                AND     year_week LIKE :yearMonth";
+        $sql = $this->dbh->prepare($req);
+        $sql->execute([
+            ':yearMonth' => $this->year . '%',
+            ':age'       => 'UNK'
+        ]);
 
-        $reqChk = "SELECT      id
+        // Requête de vérification pour éviter en entrées en doublon
+        $reqChk = " SELECT      id
                     FROM        eurostat_demo_magec
                     WHERE       sex     = :sex
                     AND         age     = :age
@@ -96,14 +91,78 @@ class demoMajecAddYear
                     AND         year    = :year";
         $sqlChk = $this->dbh->prepare($reqChk);
 
-        $reqUpd = "INSERT INTO eurostat_demo_magec
+        // Ajout de la dernière année dans "eurostat_demo_magec"
+        $reqUpd = " INSERT INTO eurostat_demo_magec
                     (unit, sex, age, geotime, year, value)
                     VALUES
                     (:unit, :sex, :age, :geotime, :year, :value)";
         $sqlUpd = $this->dbh->prepare($reqUpd);
 
-        foreach ($this->keysCountries as $country) {
+        // Boucle de compilation des données
+        $data = [];
+        while ($res = $sql->fetch()) {
 
+            $val = $res->value;
+
+            // Ratio au nb de jour de la semaine 1
+            if ($res->year_week == $this->year . 'W01') {
+                $val = $val / 7 * $this->nbDaysWeek1;
+            }
+
+            // Ratio au nb de jour de la semaine 53
+            if ($res->year_week == $this->year . 'W53') {
+                $val = $val / 7 * $this->nbDaysWeek53;
+            }
+
+            $key = $res->sex . '_' . $res->age . '_' . $res->geotime;
+            if (!isset($data[$key])) {
+                $data[$key]  = $val;
+            } else {
+                $data[$key] += $val;
+            }
         }
+
+        // Boucle d'insertion dans "eurostat_demo_magec"
+        foreach ($data as $key => $val) {
+
+            if ($val == 0) {
+                continue;
+            }
+
+            $exp = explode('_', $key);
+
+            $sex     = $exp[0];
+            $age     = $exp[1];
+            $geotime = $exp[2];
+
+            $base = [
+                ':sex'      => $sex,
+                ':age'      => $age,
+                ':geotime'  => $geotime,
+                ':year'     => $this->year
+            ];
+
+            $sqlChk->execute($base);
+
+            if ($sqlChk->rowCount() == 0) {
+                $sqlUpd->execute(array_merge($base, [':unit'=>'NR',':value'=>$val]));
+            }
+        }
+    }
+
+
+    /**
+     * Calcul automatique du nombre de jour de la 1ère et dernière semaine de l'année à traiter
+     * @return [type] [description]
+     */
+    private function nbDaysFirstLastWeek()
+    {
+        // nb de jour de la semaine 1
+        $d = new \DateTime($this->year . '-01-01');
+        $this->nbDaysWeek1 = 8 - $d->format('N');
+
+        // nb de jour de la semaine 53
+        $d = new \DateTime($this->year . '-12-31');
+        $this->nbDaysWeek53 = $d->format('N');
     }
 }
